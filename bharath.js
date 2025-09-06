7fd7e72fb3ea0fadb5bb71b758732fe027b5022f
RajithaPalla229 <pallarajitha2004.fullstack@gmail.com>
2025-09-06 22:52:34 +0530

    Initial commit

---
+++ bharath.js
@@ -0,0 +1,25 @@
+console.log("Hello world - 1");
+console.log("Hello world - 2");
+console.log("Hello world - 3");
+
+
+
+//var age = 10;
+//    age = 20;
+//console.log(age);
+
+
+// re-assign and re-declare
+// var age = 10;
+// var age = 20;
+
+
+//re-assign cannot do re-declare
+// let age = 10;
+//     age = 20;
+
+//cannot re-assign cannot do re-declare
+const age = 10;
+console.log(age);
+//document.querySelector('h1').textContent = 'Hello from bharath.js!';
+//      age = 20;
